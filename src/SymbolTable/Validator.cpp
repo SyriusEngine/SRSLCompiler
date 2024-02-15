d30cd2bc162cc219d95fe3e3f047560c422116bf
artick788 <60552965+artick788@users.noreply.github.com>
2024-02-15 10:31:31 +0100

    extended validator stage

--- src/SymbolTable/Validator.cpp
+++ src/SymbolTable/Validator.cpp
@@ -7,7 +7,13 @@ namespace Srsl{
 
     }
 
-    Validator::~Validator() {
+    Validator::~Validator() = default;
+
+    void Validator::validate() {
+        SRSL_PRECONDITION(m_InputVertexTable != nullptr, "Input vertex table is not set.");
+        SRSL_PRECONDITION(m_OutputVertexTable != nullptr, "Output vertex table is not set.");
+        SRSL_PRECONDITION(m_InputFragmentTable != nullptr, "Input fragment table is not set.");
+        SRSL_PRECONDITION(m_OutputFragmentTable != nullptr, "Output fragment table is not set.");
 
     }
 
--- src/SymbolTable/Validator.hpp
+++ src/SymbolTable/Validator.hpp
@@ -26,6 +26,8 @@ namespace Srsl{
 
         ~Validator();
 
+        void validate();
+
         template<typename... Args>
         void addConstantBuffer(Args&&... args){
             m_ConstantBuffers.emplace_back(std::forward<Args>(args)...);
@@ -41,12 +43,27 @@ namespace Srsl{
             m_Samplers.emplace_back(std::forward<Args>(args)...);
         }
 
+        void setVertexTables(RCP<SymbolTable> input, RCP<SymbolTable> output){
+            m_InputVertexTable = input;
+            m_OutputVertexTable = output;
+        }
+
+        void setFragmentTables(RCP<SymbolTable> input, RCP<SymbolTable> output){
+            m_InputFragmentTable = input;
+            m_OutputFragmentTable = output;
+        }
+
     private:
         const ShaderLimits& m_Limits;
 
         std::vector<ConstantBufferDesc> m_ConstantBuffers;
         std::vector<TextureDesc> m_Textures;
         std::vector<SamplerDesc> m_Samplers;
+
+        RCP<SymbolTable> m_InputVertexTable;
+        RCP<SymbolTable> m_OutputVertexTable;
+        RCP<SymbolTable> m_InputFragmentTable;
+        RCP<SymbolTable> m_OutputFragmentTable;
     };
 
 }
